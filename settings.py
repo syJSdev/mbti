afc09c2bd447a1116379b65e2673e4a524bf096d
ashin <ashin@hellotalk.com>
2021-11-29 09:55:17 +0800

    init fr

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ SECRET_KEY = '\xe1\x1d*\x9ft?\x19,\xdf\x8b\xa1\x10\xd0\x90\x89o)\xb0\x88\xf9\xf2
 BABEL_DEFAULT_LOCALE = 'zh'
 
 # https://www.science.co.il/language/Locale-codes.php
-SUPPORT_LANGS = ['zh', 'en', 'ko', 'de', 'ja']
+SUPPORT_LANGS = ['zh', 'en', 'ko', 'de', 'ja', 'fr']
 
 LOCALE_COOKIE_KEY = 'mbti_locale'
